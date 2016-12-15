0c2f2ca9faafefdda8ead35e9075e8fde5477e1c
Toke Jepsen <tokejepsen@gmail.com>
2016-12-15 12:05:38 +0100

    Code cosmetics

--- ftrack_connect_hooks/hook/houdini.py
+++ ftrack_connect_hooks/hook/houdini.py
@@ -11,13 +11,13 @@ import ftrack_connect.application
 
 
 class HoudiniAction(object):
-    '''Launch Houdini action.'''
+    """Launch Houdini action."""
 
     # Unique action identifier.
-    identifier = 'houdini-launch-action'
+    identifier = "houdini-launch-action"
 
     def __init__(self, applicationStore, launcher):
-        '''Initialise action with *applicationStore* and *launcher*.
+        """Initialise action with *applicationStore* and *launcher*.
 
         *applicationStore* should be an instance of
         :class:`ftrack_connect.application.ApplicationStore`.
@@ -25,54 +25,54 @@ class HoudiniAction(object):
         *launcher* should be an instance of
         :class:`ftrack_connect.application.ApplicationLauncher`.
 
-        '''
+        """
         super(HoudiniAction, self).__init__()
 
         self.logger = logging.getLogger(
-            __name__ + '.' + self.__class__.__name__
+            __name__ + "." + self.__class__.__name__
         )
 
         self.applicationStore = applicationStore
         self.launcher = launcher
 
         if self.identifier is None:
-            raise ValueError('The action must be given an identifier.')
+            raise ValueError("The action must be given an identifier.")
 
     def register(self):
-        '''Register discover actions on logged in user.'''
+        """Register discover actions on logged in user."""
         ftrack.EVENT_HUB.subscribe(
-            'topic=ftrack.action.discover and source.user.username={0}'.format(
+            "topic=ftrack.action.discover and source.user.username={0}".format(
                 getpass.getuser()
             ),
             self.discover
         )
 
         ftrack.EVENT_HUB.subscribe(
-            'topic=ftrack.action.launch and source.user.username={0} '
-            'and data.actionIdentifier={1}'.format(
+            "topic=ftrack.action.launch and source.user.username={0} "
+            "and data.actionIdentifier={1}".format(
                 getpass.getuser(), self.identifier
             ),
             self.launch
         )
 
     def is_valid_selection(self, selection):
-        '''Return true if the selection is valid.'''
+        """Return true if the selection is valid."""
         if (
             len(selection) != 1 or
-            selection[0]['entityType'] != 'task'
+            selection[0]["entityType"] != "task"
         ):
             return False
 
         entity = selection[0]
-        task = ftrack.Task(entity['entityId'])
+        task = ftrack.Task(entity["entityId"])
 
-        if task.getObjectType() != 'Task':
+        if task.getObjectType() != "Task":
             return False
 
         return True
 
     def discover(self, event):
-        '''Return available actions based on *event*.
+        """Return available actions based on *event*.
 
         Each action should contain
 
@@ -83,42 +83,42 @@ class HoudiniAction(object):
             applicationIdentifier - Unique identifier to identify application
                                     in store.
 
-        '''
+        """
         if not self.is_valid_selection(
-            event['data'].get('selection', [])
+            event["data"].get("selection", [])
         ):
             return
 
         items = []
         applications = self.applicationStore.applications
         applications = sorted(
-            applications, key=lambda application: application['label']
+            applications, key=lambda application: application["label"]
         )
 
         for application in applications:
-            applicationIdentifier = application['identifier']
-            label = application['label']
+            applicationIdentifier = application["identifier"]
+            label = application["label"]
             items.append({
-                'actionIdentifier': self.identifier,
-                'label': label,
-                'variant': application.get('variant', None),
-                'description': application.get('description', None),
-                'icon': application.get('icon', 'default'),
-                'applicationIdentifier': applicationIdentifier
+                "actionIdentifier": self.identifier,
+                "label": label,
+                "variant": application.get("variant", None),
+                "description": application.get("description", None),
+                "icon": application.get("icon", "default"),
+                "applicationIdentifier": applicationIdentifier
             })
 
         return {
-            'items': items
+            "items": items
         }
 
     def launch(self, event):
-        '''Callback method for Houdini action.'''
+        """Callback method for Houdini action."""
         applicationIdentifier = (
-            event['data']['applicationIdentifier']
+            event["data"]["applicationIdentifier"]
         )
 
-        context = event['data'].copy()
-        context['source'] = event['source']
+        context = event["data"].copy()
+        context["source"] = event["source"]
 
         return self.launcher.launch(applicationIdentifier, context)
 
@@ -126,49 +126,49 @@ class HoudiniAction(object):
 class ApplicationStore(ftrack_connect.application.ApplicationStore):
 
     def _discoverApplications(self):
-        '''Return a list of applications that can be launched from this host.
+        """Return a list of applications that can be launched from this host.
 
         An application should be of the form:
 
             dict(
-                'identifier': 'name_version',
-                'label': 'Name version',
-                'path': 'Absolute path to the file',
-                'version': 'Version of the application',
-                'icon': 'URL or name of predefined icon'
+                "identifier": "name_version",
+                "label": "Name version",
+                "path": "Absolute path to the file",
+                "version": "Version of the application",
+                "icon": "URL or name of predefined icon"
             )
 
-        '''
+        """
         applications = []
-        icon = 'http://cl.ly/image/3v2f3h2c0H3G/h_logo.png'
+        icon = "http://cl.ly/image/3v2f3h2c0H3G/h_logo.png"
 
-        if sys.platform == 'darwin':
-            prefix = ['/', 'Applications']
+        if sys.platform == "darwin":
+            prefix = ["/", "Applications"]
 
             applications.extend(self._searchFilesystem(
                 expression=prefix + [
-                    'Houdini*', 'Houdini.app'
+                    "Houdini*", "Houdini.app"
                 ],
-                label='Houdini',
-                variant='{version}',
-                applicationIdentifier='houdini_{version}',
+                label="Houdini",
+                variant="{version}",
+                applicationIdentifier="houdini_{version}",
                 icon=icon
             ))
 
-        elif sys.platform == 'win32':
-            prefix = ['C:\\', 'Program Files.*']
+        elif sys.platform == "win32":
+            prefix = ["C:\\", "Program Files.*"]
 
             applications.extend(self._searchFilesystem(
-                expression=prefix + ['Side Effects Software', 'Houdini*',
-                                     'bin', 'houdini.exe'],
-                label='Houdini',
-                variant='{version}',
-                applicationIdentifier='houdini_{version}',
+                expression=prefix + ["Side Effects Software", "Houdini*",
+                                     "bin", "houdini.exe"],
+                label="Houdini",
+                variant="{version}",
+                applicationIdentifier="houdini_{version}",
                 icon=icon
             ))
 
         self.logger.debug(
-            'Discovered applications:\n{0}'.format(
+            "Discovered applications:\n{0}".format(
                 pprint.pformat(applications)
             )
         )
@@ -177,7 +177,7 @@ class ApplicationStore(ftrack_connect.application.ApplicationStore):
 
 
 def register(registry, **kw):
-    '''Register hooks.'''
+    """Register hooks."""
 
     # Validate that registry is the correct ftrack.Registry. If not,
     # assume that register is being called with another purpose or from a
@@ -199,7 +199,7 @@ def register(registry, **kw):
     action.register()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig()
     logging.getLogger().setLevel(logging.INFO)
 
@@ -221,11 +221,11 @@ if __name__ == '__main__':
     import app_launch_environment
 
     ftrack.EVENT_HUB.subscribe(
-        'topic=ftrack.connect.application.launch',
+        "topic=ftrack.connect.application.launch",
         app_launch_open_file.modify_application_launch)
 
     ftrack.EVENT_HUB.subscribe(
-        'topic=ftrack.connect.application.launch',
+        "topic=ftrack.connect.application.launch",
         app_launch_environment.modify_application_launch)
 
     ftrack.EVENT_HUB.wait()
